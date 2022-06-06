4ff1c8ae7063475c4842c893f35c27eb5adaecd9
Li Ka Lok <75416686+kaloklijk@users.noreply.github.com>
2022-06-06 15:43:07 +0800

    Update stocks.py

--- stocks.py
+++ stocks.py
@@ -1,9 +1,7 @@
 import yfinance as yf
 import streamlit as st
 import pandas as pd
-from get_all_tickers import get_tickers as gt
 import numpy as np
-from collections import defaultdict
 # get tickers symbols
 with open("symbol.txt", "r") as f:
     # all tickers
@@ -11,7 +9,6 @@ with open("symbol.txt", "r") as f:
     st.write(f"""# stocks prices
     Closing data for all stocks prices
     """)
-    prices = []
     for ticker in tickerList:
         tickerData = yf.Ticker(ticker)
         #np.append(prices, tickerData.history().Close[-1])
