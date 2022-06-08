95b43d8af0c6234b2af2cc32bfe993c5daee88ad
Li Ka Lok <75416686+kaloklijk@users.noreply.github.com>
2022-06-08 13:57:45 +0800

    fix bugs

--- search_stocks.py
+++ search_stocks.py
@@ -2,18 +2,24 @@ import numpy as np
 import streamlit as st
 import yfinance as yf
 import pandas as pd
+from collections import defaultdict
 st.write("""
 # stocks
 """)
 with open("symbol.txt", "r") as f:
     tickers = f.read().split(",")
     selects = st.multiselect("stocks symbols", tickers)
+    startdate = defaultdict()
+    enddate = defaultdict()
     for ticker in selects:
         st.write(f"""
         # {ticker}
         """)
-        startdate = st.date_input("start")
-        enddate = st.date_input("end")
+        try:
+            startdate[ticker] = st.date_input("start")
+            enddate[ticker] = st.date_input("end")
+        except Exception:
+            pass
         ticker_price = yf.Ticker(ticker).history(period="1d", start=startdate, end=enddate).Close
         ticker_analysis = yf.download(ticker, start=startdate, end=enddate)
 
