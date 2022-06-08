aac32a5bb98984af9065fc83d59c1f0b4cab2765
Li Ka Lok <75416686+kaloklijk@users.noreply.github.com>
2022-06-08 13:18:26 +0800

    update searchstocks

--- search_stocks.py
+++ search_stocks.py
@@ -1,7 +1,7 @@
 import numpy as np
 import streamlit as st
 import yfinance as yf
-
+import pandas as pd
 st.write("""
 # stocks
 """)
@@ -9,17 +9,22 @@ with open("symbol.txt", "r") as f:
     tickers = f.read().split(",")
     selects = st.multiselect("stocks symbols", tickers)
     for ticker in selects:
-        ticker_price = yf.Ticker(ticker).history(period="1d", start='2018-1-1').Close
-        ticker_analysis = yf.Ticker(ticker).analysis
         st.write(f"""
         # {ticker}
         """)
+        startdate = st.date_input("start")
+        enddate = st.date_input("end")
+        ticker_price = yf.Ticker(ticker).history(period="1d", start=startdate, end=enddate).Close
+        ticker_analysis = yf.download(ticker, start=startdate, end=enddate)
+
         st.write(f"""
         ## Recent Close Price: {ticker_price[-1]}
 
         """)
+        st.line_chart(ticker_price)
         st.write(f"""
         ## stocks analysis: 
-        {ticker_analysis}
+        
         """)
-        st.line_chart(ticker_price)
+        st.table(ticker_analysis)
+  
